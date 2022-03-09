1a8298972f3bb29a074d79857db3737ea35f3086
geraldkachi <fitzgeraldkachi@gmail.com>
2022-03-09 13:18:55 +0100

    update on Nav

--- src/admin-dashboard/components/Dashboard.tsx
+++ src/admin-dashboard/components/Dashboard.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+import Analytics from './Analytics'
+import Earnings from './Earnings'
+import FAQ from './FAQ'
+import Navbar from './Navbar'
+import Profile from './Profile'
+import Transfers from './Transfers'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Section>
+      <Navbar />
+      <div className="row__one">
+        <Analytics />
+        <FAQ />
+      </div>
+      <div className="row__two">
+        <Earnings />
+        <Transfers />
+        <Profile />
+      </div>
+    </Section>
   )
 }
 
 export default Dashboard
+
+const Section = styled.div`
+`
--- src/admin-dashboard/components/Navbar.tsx
+++ src/admin-dashboard/components/Navbar.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { BiSearch } from "react-icons/bi"
+import styled from 'styled-components'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Nav>
+        <div className="title">
+          <h4>Hi Gerald Kachi</h4>
+          <h1>Welcome to <span>MY TAXI DASHBOARD</span></h1>
+        </div>
+        <div className="search">
+          <BiSearch />
+          <input type="text"placeholder="Search..." />
+        </div>
+    </Nav>
   )
 }
 
 export default Navbar
+
+const Nav = styled.div`
+  
+`
