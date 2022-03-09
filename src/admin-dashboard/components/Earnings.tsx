b5a0d6f97a10ddcf4ce5f55e02485d46064b972f
geraldkachi <fitzgeraldkachi@gmail.com>
2022-03-09 23:56:14 +0100

    on tranfers

--- src/admin-dashboard/components/Earnings.tsx
+++ src/admin-dashboard/components/Earnings.tsx
@@ -1,11 +1,89 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { AreaChart, Area, Tooltip, ResponsiveContainer } from "recharts";
+import { data } from "../data./chart";
+import { cardStyle } from "./ReusableStyles";
+
 
 const Earnings = () => {
   return (
-    <div>
-        Earnings
+    <Section>
+      <div className="top">
+        <div className="info">
+          <h5>This month earnings</h5>
+          <h1>$682.5</h1>
+          <div className="growth">
+            <span>+2.45%</span>
+          </div>
         </div>
+      </div>
+      <div className="chart">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
+          >
+            <Tooltip cursor={false} />
+            <Area
+              animationBegin={800}
+              animationDuration={2000}
+              type="monotone"
+              dataKey="data"
+              stroke="#ffc107"
+              fill="#8068233e"
+              strokeWidth={4}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </Section>
   )
 }
 
 export default Earnings
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 20rem;
+  ${cardStyle}
+  padding: 2rem 0 0 0;
+  .top {
+    .info {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.2rem;
+      h1 {
+        font-size: 2rem;
+      }
+      .growth {
+        background-color: #d7e41e1d;
+        padding: 0.5rem;
+        border-radius: 1rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          background-color: #ffc107;
+          span {
+            color: black;
+          }
+        }
+        span {
+          color: #ffc107;
+        }
+      }
+    }
+  }
+  .chart {
+    height: 70%;
+    .recharts-default-tooltip {
+      background-color: black !important;
+      border-color: black !important;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+  }
+`;
--- src/admin-dashboard/components/Transfers.tsx
+++ src/admin-dashboard/components/Transfers.tsx
@@ -1,9 +1,109 @@
 import React from 'react'
+import styled from "styled-components";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import avatarImage from "../assets/avatarImage.jpeg";
+import { cardStyle } from "./ReusableStyles";
+import { Link } from 'react-router-dom';
 
+  const transactions = [
+    {
+      image: avatarImage,
+      name: "From Kishan Sheth",
+      time: "Today, 16:36",
+      amount: "+$50",
+    },
+    {
+      image: avatarImage,
+      name: "To Lauras Santos",
+      time: "Today, 08:49",
+      amount: "-$25",
+    },
+    {
+      image: avatarImage,
+      name: "From Jadon S.",
+      time: "Yesterday, 14:36",
+      amount: "+$150",
+    },
+  ];
 const Transfers = () => {
   return (
-    <div>Transfers</div>
+    <Section>
+      <div className="title">
+        <h2>Your Transfers</h2>
+      </div>
+      <div className="transactions">
+        {transactions.map(item=> (
+          <div className="transaction">
+            <div className="transaction__title">
+              <div className="transaction__title__image">
+                <img src={item.image} alt={item.name} />
+              </div>
+              <div className="transaction__title__details">
+                <h3>{item.name}</h3>
+                <h3>{item.time}</h3>
+              </div>
+              <div className="transaction__amount">
+                {item.amount}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Link to="" className="view">
+            View all <HiArrowNarrowRight />
+      </Link>
+      </Section>
   )
 }
 
 export default Transfers
+
+const Section = styled.div`
+    ${cardStyle}
+    display: flex;
+    flex-flow: column;
+    gap: 1rem;
+
+    .title{
+      h2 {
+        color: #ffc107;
+        font-family: "Permanent Marker", cursive;
+        letter-spacing: .3rem;
+      }
+    }
+    .transactions {
+      display: flex;
+      flex-flow: column;
+      gap: 1rem;
+      margin-top: 1rem;
+        .transaction {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          &__title {
+            display: flex;
+            gap: 1rem;
+              &__image {
+                img {
+                  height: 2.5rem;
+                  border-radius: 3rem;
+                }
+              }
+          }
+          &__amount {
+            background-color: #d7e41e1d;
+            padding: .2rem, .5rem;
+            width: 4rem;
+             text-align: center;
+             transition: 0ms.3s ease-in-out;
+             &::hover {
+               background-color: #ffc107;
+               span {
+                 color: #000;
+               }
+             }
+          }
+        }
+    }
+    
+`
