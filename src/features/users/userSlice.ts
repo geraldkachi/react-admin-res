66a3c9a8afb32abcfc87034f3c9eba6bfcbb6e12
geraldkachi <fitzgeraldkachi@gmail.com>
2022-03-11 16:58:55 +0100

    updated

--- src/features/users/userSlice.ts
+++ src/features/users/userSlice.ts
@@ -1,5 +1,5 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState, AppThunk } from '../../../../redux-crud/src/app/store';
+import {  createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../../../redux-crud/src/app/store';
 import { UserDataProps, UsersData } from '../../../../redux-crud/src/fetchData';
 
 export interface CounterState {
